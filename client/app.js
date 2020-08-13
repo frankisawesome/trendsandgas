7baacaeeeda9ca4255201fd58d58ac1db315eb48
Frank <b439988l@gmail.com>
2020-08-13 21:34:59 +1000

    added emoji

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Graph from './graph'
 import './index.css'
+import gaspng from './gas.png'
 
 const App = () => {
   const [address, setAddress] = useState()
@@ -27,15 +28,16 @@ const App = () => {
   ]
 
   return (<div className="w-full flex flex-col items-center">
+    <img src={gaspng} height="150" width="150" className="py-6" />
     <h1 className="text-4xl font-semibold py-10 text-red-400">How much gas did I pay?</h1>
-    <div className="max-w-xl w-full md:w-3/5 flex flex-col items-center mt-10">
+    <div className="max-w-xl w-full md:w-3/5 flex flex-col items-center mt-8">
       <input 
       className="bg-red-100 appearance-none border-2 border-red-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-red-400 shadow-2xl" 
       value={address}
       onChange={(e) => setAddress(e.target.value)}
       />
       <div>
-        <button onClick={() => getData()}className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10 mx-6">gaaaas?</button>
+        <button onClick={() => getData()}className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10 mx-8">gaaaas?</button>
         <button onClick={() => connect()} className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10">get address from metamask</button>
       </div>
     </div>
