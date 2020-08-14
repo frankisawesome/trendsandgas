56c8e3967ff9f531e408ec5b7bcecc05f9d09dc8
Frank <b439988l@gmail.com>
2020-08-14 22:14:56 +1000

    added slide api from material ui

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Graph from './graph'
 import './index.css'
 import gaspng from './gas.png'
+import Slide from '@material-ui/core/Slide';
 
 const App = () => {
   const [address, setAddress] = useState()
@@ -27,22 +28,29 @@ const App = () => {
     { gas_price: 201 },
   ]
 
-  return (<div className="w-full flex flex-col items-center">
-    <img src={gaspng} height="150" width="150" className="py-6" />
-    <h1 className="text-4xl font-semibold py-10 text-red-400">How much gas did I pay?</h1>
-    <div className="max-w-xl w-full md:w-3/5 flex flex-col items-center mt-8">
-      <input 
-      className="bg-red-100 appearance-none border-2 border-red-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-red-400 shadow-2xl" 
-      value={address}
-      onChange={(e) => setAddress(e.target.value)}
-      />
-      <div>
-        <button onClick={() => getData()}className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10 mx-8">gaaaas?</button>
-        <button onClick={() => connect()} className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10">get address from metamask</button>
-      </div>
+  return (<div className=" flex flex-col items-center min-h-screen">
+    <div className="flex-1 w-full flex flex-col items-center">
+      <img src={gaspng} height="150" width="150" className="py-10" />
+        <h1 className="text-4xl font-semibold pt-10 text-red-400">How much gas did I pay?</h1>
+        <Slide direction="up" in={!data} mountOnEnter unmountOnExit>
+          <div className="max-w-xl w-full md:w-3/5 flex flex-col items-center mt-8">
+            <input 
+            className="bg-red-100 appearance-none border-2 border-red-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-red-400 shadow-2xl" 
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            />
+            <div>
+              <button onClick={() => getData()}className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10 mx-8">gaaaas?</button>
+              <button onClick={() => connect()} className="bg-red-400 shadow-xl text-white rounded px-4 py-2 focus:outline-none focus:bg-red-200 hover:bg-white hover:border-red-400 hover:text-red-400 mt-10">get address from metamask</button>
+            </div>
+          </div>
+        </Slide>
+      <Graph data={data}/>
     </div>
-    <Graph data={data}/>
-  </div>)
+      <footer className="flex items-center justify-center flex-shrink-0 pb-4">
+        <a className="text-red-400 font-medium" href="https://github.com/frankisawesome/trendsandgas">Github</a>
+      </footer>
+    </div>)
 }
 
 export default App
