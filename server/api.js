2340127c4116830e1a62a1e9be877d28e9269181
Frank <b439988l@gmail.com>
2020-08-15 15:58:46 +1000

    data processor made on backend

--- server/api.js
+++ server/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const axios = require('axios')
-const { getBlockInterval } = require('./services')
+const { getBlockInterval, processResults } = require('./services')
 
 router.get('/', (req, res) => {
   res.send('Gas trends api')
@@ -11,7 +11,7 @@ router.get('/gas/:address', async (req, res) => {
     const address = req.params.address
     const [from, to] = await getBlockInterval(req.query.filter)
     const data = await axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=${from}&endblock=${to}&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`)
-    res.send(data.data.result)
+    res.send(processResults(data.data.result))
   } catch (e) {
     res.send({ error: true, message: e.message ? e.message : e })
   }
--- server/services.js
+++ server/services.js
@@ -1,10 +1,14 @@
 const axios = require('axios')
 
-module.exports = { getBlockInterval }
+module.exports = { getBlockInterval, processResults }
+
+//multiplier applied on gwei for transactions
+const GWEI_MULTIPLIER = 1000000000
 
 //get block numbers for intervals (day/week/month)
 async function getBlockInterval(filter) {
   const to = new Date()
+  //get latest block
   const data = await axios.get(`https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${parseInt(to.getTime() / 1000)}&closest=before&apikey=${process.env.ETHERSCAN_KEY}`)
   const latestBlock = data.data.result
   let from = new Date()
@@ -17,8 +21,44 @@ async function getBlockInterval(filter) {
   } else {
     throw { error: 'true', message: 'incorrect filter'}
   }
+  //get starting block
   const fromdata = await axios.get(`https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${parseInt(from.getTime() / 1000)}&closest=before&apikey=${process.env.ETHERSCAN_KEY}`)
   const fromBlock = fromdata.data.result
 
   return [fromBlock, latestBlock]
+}
+
+//takes an array of transactions data from etherscan api and return in our desired format
+function processResults(results) {
+  //process this on the front end - no transactions made in the time period
+  if (results.length === 0) {
+    return {
+      data: null,
+      error: true,
+      message: 'No results'
+    }
+  }
+  //loop through the results, find points of interests as well as pushing the price points into a new array
+  const data = []
+  let sum = 0, lowest = Infinity
+  let highest = 0
+  results.forEach((transaction) => {
+    const gasPrice = parseInt(transaction.gasPrice / GWEI_MULTIPLIER)
+    sum += gasPrice
+    if (gasPrice < lowest) {
+      lowest = gasPrice
+    }
+    if (gasPrice > highest) {
+      highest = gasPrice
+    }
+    //wrap the gas price in an object to suit recharts data shape requirement
+    data.push({ gasPrice })
+  })
+  //return the data with the correct multiplier applied
+  return {
+    data: data,
+    avg: sum / results.length,
+    lowest: lowest,
+    highest: highest
+  }
 }
