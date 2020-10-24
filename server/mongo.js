fef048b5501b89b8387aa5e5b1333b9f7a54ff57
Frank <b439988l@gmail.com>
2020-10-24 13:35:47 +1000

    updated mongo logic to check for date

--- server/mongo.js
+++ server/mongo.js
@@ -1,7 +1,9 @@
 //mongo layer middlewares (persistence)
 const mongoMiddlewareGas = (req, res, next) => {
+  //3 hours ago
+  const time = Date.now() - 10800000
   const key = `gas/${req.params.address}-${req.query.filter}`
-  req.mongo.db('cab432').collection('results').findOne({ id: key }, (err, data) => {
+  req.mongo.db('cab432').collection('results').findOne({ id: key, timestamp: { $gte: time } }, (err, data) => {
     if (data) {
       console.log(`[INFO] MongoDB hit for query ${key}`)
       req.redis.setex(key, 600, JSON.stringify(data.data))
@@ -14,8 +16,10 @@ const mongoMiddlewareGas = (req, res, next) => {
 }
 
 const mongoMiddlewareStats = (req, res, next) => {
+  //3 hours ago
+  const time = Date.now() - 10800000
   const key = `eth/stats-${req.query.filter}`
-  req.mongo.db('cab432').collection('results').findOne({ id: key }, (err, data) => {
+  req.mongo.db('cab432').collection('results').findOne({ id: key, timestamp: { $gte: time } }, (err, data) => {
     if (data) {
       console.log(`[INFO] MongoDB hit for query ${key}`)
       req.redis.setex(key, 600, JSON.stringify(data.data))
